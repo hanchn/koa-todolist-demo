0a95426bd8e9ad803295f9c7482a9bc6df48c129
yuanjing <jing.yuan@kapeixi.com>
2025-01-14 11:44:54 +0800

    .

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // 以 Koa + koa-views + EJS 举例
 import Koa from 'koa';
-import views from 'koa-views';
+import bodyParser from 'koa-bodyparser';
+import render from 'koa-ejs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 // 以下两行是为了获取当前文件的目录名(__dirname在ESM中不可直接用)。
@@ -8,11 +9,29 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = new Koa();
+// 错误事件监听
+app.on('error', (err, ctx) => {
+  console.error('Server Error:', err);
+});
+// 全局中间件
+app.use(bodyParser());
 
-app.use(views(path.join(__dirname, 'views'), { extension: 'ejs' }));
+// 配置 koa-ejs
+render(app, {
+  root: path.join(__dirname, '/', 'views'),
+  layout: false, // 如果使用布局模板，可以设置为布局文件名
+  viewExt: 'ejs',
+  cache: false,
+  debug: false
+});
 
 app.use(async (ctx) => {
-  await ctx.render('index', { title: 'Hello Koa EJS' });
+  const { request: { url } } = ctx;
+  if (url === '/') {
+    await ctx.render('index', { title: 'Hello Koa EJS' });
+  } else { 
+    ctx.body = '404';
+  }
 });
 
 export default app;
